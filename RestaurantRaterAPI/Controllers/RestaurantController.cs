f2a389cf44483f1bb834cd458de97facba4961bb
Sabra Snider <sabralsnider@gmail.com>
2020-05-28 10:11:26 -0400

    first commit

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,28 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+        [HttpPost]
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; } //2 caps when just ID but if you have a word before it, d is lowercase, and it needs to be uniform throughout.
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public double Rating { get; set; }
+    }
+}
